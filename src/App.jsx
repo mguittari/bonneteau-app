c73c73561cce493abb8dfe504d164bebbb81dac5
mguittari <mattias.guittari@gmail.com>
2025-01-15 09:43:40 +0100

    algo fisher yates for change array elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import PlayMat from "./Components/PlayMat/PlayMat";
 
 function App() {
+	const [figures, setFigures] = useState(["circle", "square", "triangle"]);
+
+	const shuffle = (array) => {
+		const shuffled = array.sort(() => Math.random() - 0.5);
+		setFigures(shuffled);
+	};
+
+	useEffect(() => {
+		shuffle(figures);
+		console.log("figures -->", figures);
+	});
+
 	return (
 		<>
 			<PlayMat />
